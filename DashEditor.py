5dce1ba221e0f9809114bd72cde9da3af13ec752
OmbraRD <64612056+OmbraRD@users.noreply.github.com>
2020-06-01 02:17:55 -0400

    Refactor of file handling. Fixed compatibility with linux. Fixed byte-swapping

--- DashEditor.py
+++ DashEditor.py
@@ -1,62 +1,6 @@
-import sys, os, re, math
+#!/usr/bin/env python3
 
-
-def do_pack_bin():
-    # Initialize the needed variables
-    index_file_item = 0
-
-    # Check if all files inside the index exists and are the correct size
-    error = False
-
-    while index_file_item < len(index_file_data):
-
-        path_file_to_write = str(folder_name + "/" +
-                                 index_file_data[index_file_item]).replace("\\n", "").split(",")[0]
-        file_to_write_index_len = str(index_file_data[index_file_item]).replace("\\n", "").split(",")[1]
-        # If the path does not exist. ERROR
-        if not os.path.exists(path_file_to_write):
-            print(path_file_to_write + " does not exists")
-            index_file_item += 1
-            error = True
-        # If the file sizes don't match. ERROR
-        elif os.path.getsize(path_file_to_write) != int(file_to_write_index_len):
-            print(path_file_to_write + " has a size of " + str(os.path.getsize(path_file_to_write)) +
-                  " instead of " + file_to_write_index_len + " bytes")
-            index_file_item += 1
-            error = True
-        else:
-            index_file_item += 1
-
-    # If all the tests pass,
-    if not error:
-        print("Creating " + folder_name + ".BIN file...")
-        output_bin = open(folder_name + ".BIN", "ab")
-
-        index_file_item = 0
-        end_file_size = 0
-        padding = 2048  # 0x800
-
-        while index_file_item < len(index_file_data):
-            # Get file path and open it
-            path_file_to_write = str(folder_name + "/" +
-                                     index_file_data[index_file_item]).replace("\\n", "").split(",")[0]
-            file_to_write = open(path_file_to_write, "rb")
-            # Write the contents of opened file to destination BIN and close opened file
-            output_bin.write(file_to_write.read())
-            file_to_write.close()
-            # Add the written file size to a variable for use with padding calculations
-            end_file_size = end_file_size + os.path.getsize(path_file_to_write)
-            index_file_item += 1
-
-        # Add Archive termination before padding
-        output_bin.write(b'\xFF\xFF\xFF\xFF')
-
-        # Calculate end padding and write it
-        end_padding = (((end_file_size // padding) + 1) * padding) - end_file_size - 4
-        output_bin.write(end_padding * b'\x00')
-
-        output_bin.close()
-    return
+import os, re, sys
 
 
 def do_unpack_bin():
@@ -69,7 +13,6 @@ def do_unpack_bin():
 
     # Check if the amount of extracted files is correct
     loop_counter = 0
-    existing_file_counter = 0
 
     while offset < len(file_data):
 
@@ -79,30 +22,17 @@ def do_unpack_bin():
             # Check if if it starts with a ..\
             if file_data[offset + 64:offset + 128].startswith(b"..\\"):
 
-                # Get folder and filename
-                file_name_and_folders: str = file_data[offset + 64:offset + 128].replace(b'\x00', b'').decode()
+                # Get folders and filename and replace \ with / (Fix for Linux since os.path.norpath does not work)
+                inner_folder_and_file: str = (
+                    file_data[offset + 67:offset + 128].replace(b'\x00', b'').decode().replace("\\", "/"))
 
                 # Get folders names
-                for folders in [file_name_and_folders.split("\\")]:
+                for folders in [inner_folder_and_file.split("/")]:
                     # Skip all items that contains a dot (beginning and filename)
-                    full_path = file_path + "/" + "/".join([x for x in folders if not "." in x])
+                    full_path = full_path_and_file_no_ext + "/" + "/".join([x for x in folders if not "." in x])
                     # Check if the folder has already been created. If not, create it
                     if not os.path.exists(full_path):
                         os.makedirs(full_path)
-
-                # Get file name and extension
-                inner_file_name = file_name_and_folders.split("\\")[-1]
-                inner_file_extension = inner_file_name.split(".")[-1].upper()
-                file_name_to_write = full_path + "/" + inner_file_name
-
-                # Check if there are files with the same names (it happens). Add number to name and get ready for write
-                if os.path.exists(file_name_to_write):
-                    file_name_to_write = (
-                            full_path + "/" + inner_file_name.split(".")[0] + "_"
-                            + str(existing_file_counter) + "." + inner_file_name.split(".")[-1])
-                    inner_file = open(file_name_to_write, "wb")
-                else:
-                    inner_file = open(file_name_to_write, "wb")
 
                 # Check file type for extraction
 
@@ -117,62 +47,60 @@ def do_unpack_bin():
 
                 # Size = Width (0x24) * Height (0x28) * 2
                 # Regular TIM
-
                 if file_data[offset:offset + 4] == b'\x01\x00\x00\x00':  # TIM
-                    tim_width = int.from_bytes(
-                        [file_data[offset + 39], file_data[offset + 38],
-                         file_data[offset + 37], file_data[offset + 36]], byteorder="big"
-                    )
-                    tim_height = int.from_bytes(
-                        [file_data[offset + 43], file_data[offset + 42],
-                         file_data[offset + 41], file_data[offset + 40]], byteorder="big"
-                    )
+                    tim_width = int.from_bytes(file_data[offset + 36:offset + 40], byteorder="little")
+                    tim_height = int.from_bytes(file_data[offset + 40:offset + 44], byteorder="little")
                     file_len = tim_width * tim_height * 2
 
                 # CLT, Imageless TIM with CLUT only or Imageless TIM with CLUT Patch
                 # Size = Width (0x14) * Height (0x18) * 2
-
                 elif ((file_data[offset:offset + 4] == b'\x04\x00\x00\x00') or  # CLT
                       (file_data[offset:offset + 4] == b'\x09\x00\x00\x00') or  # Imageless CLUT (in TIM format)
                       (file_data[offset:offset + 4] == b'\x0A\x00\x00\x00')):  # Imagelesss CLUT Patch(in TIM format)
-                    clt_tim_width = int.from_bytes(
-                        [file_data[offset + 23], file_data[offset + 22],
-                         file_data[offset + 21], file_data[offset + 20]], byteorder="big"
-                    )
-                    clt_tim_height = int.from_bytes(
-                        [file_data[offset + 27], file_data[offset + 26],
-                         file_data[offset + 25], file_data[offset + 24]], byteorder="big"
-                    )
+                    clt_tim_width = int.from_bytes(file_data[offset + 20:offset + 24], byteorder="little")
+                    clt_tim_height = int.from_bytes(file_data[offset + 24:offset + 28], byteorder="little")
                     file_len = clt_tim_width * clt_tim_height * 2
 
                 # FONT
                 elif file_data[offset:offset + 4] == b'\x03\x00\x00\x00':  # FONT
-                    file_len = int.from_bytes(
-                        [file_data[offset + 7], file_data[offset + 6],
-                         file_data[offset + 5], file_data[offset + 4]], byteorder="big"
-                        ) + 1
+                    file_len = int.from_bytes(file_data[offset + 4:offset + 8], byteorder="little") + 1
 
                 # Anything else.
                 else:
-                    file_len = int.from_bytes(
-                        [file_data[offset + 7], file_data[offset + 6],
-                         file_data[offset + 5], file_data[offset + 4]], byteorder="big"
-                    ) + padding
+                    file_len = int.from_bytes(file_data[offset + 4:offset + 8], byteorder="little") + padding
+
                     padded_file_size = ((file_len // padding) + 1) * padding
                     # This fixes files that already have an aligned size or that still have data in the middle
                     if file_data[offset + file_len + 64:offset + file_len + 67].startswith(b"..\\"):
                         file_len = file_len - 1
-                    elif not (
-                            file_data[offset + padded_file_size + 64:offset + padded_file_size + 67].startswith(b"..\\") or
-                            file_data[offset + padded_file_size:offset + padded_file_size + 4].startswith(b"\xFF\xFF\xFF\xFF")):
+                    elif not (file_data[offset + padded_file_size + 64:offset + padded_file_size + 67].startswith(
+                            b"..\\") or
+                              file_data[offset + padded_file_size:offset + padded_file_size + 4].startswith(
+                                  b"\xFF\xFF\xFF\xFF")):
                         file_len = file_len + padding
 
                 # Add the file length to the offset and align to 0x800. If it is already a multiple add 1
                 padded_file_size = ((file_len // padding) + 1) * padding
 
-                print("Extracting " + inner_file_name + " to folder /" + full_path)
-                print("Real file size: " + str(file_len) + " byes / Aligned file size: "
-                      + str(padded_file_size) + " bytes.")
+                # Get file name and extension
+                inner_file_name = inner_folder_and_file.split("/")[-1]
+
+                # Check if there are files with the same names (it happens). Add number to name and get ready for write
+                inner_file_name_only = inner_file_name.split(".")[0]
+                inner_file_name_extension = inner_file_name.split(".")[-1]
+
+                file_name_to_write = full_path + "/" + inner_file_name
+
+                existing_file_counter = 0
+                if os.path.exists(file_name_to_write):
+                    file_name_to_write = (full_path + "/" + inner_file_name_only + "_"
+                                          + str(existing_file_counter) + "." + inner_file_name_extension)
+
+                inner_file = open(file_name_to_write, "wb")
+
+                print("Extracting " + inner_file_name + " to folder " + os.path.dirname(file_name_to_write))
+                print("Real file size: " + str(file_len) + " byes - " +
+                      "Aligned file size: " + str(padded_file_size) + " bytes.")
 
                 # Write file
                 inner_file.write(file_data[offset:offset + padded_file_size])
@@ -180,7 +108,7 @@ def do_unpack_bin():
 
                 # Write file info to index file for packing later
                 index_file.write(
-                    str(file_name_to_write.replace(file_path + "/", "")) + "," + str(padded_file_size) + "\n")
+                    file_name_to_write.replace(full_path_and_file_no_ext + "/", "") + "," + str(padded_file_size) + "\n")
 
                 # Set offset to the old offset + the size of the aligned files
                 offset = offset + padded_file_size
@@ -200,6 +128,65 @@ def do_unpack_bin():
     return
 
 
+def do_pack_bin():
+    # Initialize the needed variables
+    index_file_item: int = 0
+
+    # Check if all files inside the index exists and are the correct size
+    error = False
+
+    while index_file_item < len(index_file_data):
+
+        path_inner_file_to_write = (
+            full_file_or_folder_name + "/" + index_file_data[index_file_item].replace("\\n", "").split(",")[0])
+        file_to_write_index_len = index_file_data[index_file_item].replace("\\n", "").split(",")[1]
+
+        # If the path does not exist. ERROR
+        if not os.path.exists(path_inner_file_to_write):
+            print(path_inner_file_to_write + " does not exists")
+            index_file_item += 1
+            error = True
+        # If the file sizes don't match. ERROR
+        elif os.path.getsize(path_inner_file_to_write) != int(file_to_write_index_len):
+            print(path_inner_file_to_write + " has a size of " + str(os.path.getsize(path_inner_file_to_write)) +
+                  " instead of " + file_to_write_index_len + " bytes")
+            index_file_item += 1
+            error = True
+        else:
+            index_file_item += 1
+
+    # If all the tests pass,
+    if not error:
+        print("Creating " + full_file_or_folder_name + ".BIN file...")
+        output_bin = open(full_file_or_folder_name + ".BIN", "ab")
+
+        index_file_item = 0
+        end_file_size = 0
+        padding = 2048  # 0x800
+
+        while index_file_item < len(index_file_data):
+            # Get file path and open it
+            path_inner_file_to_write = (
+                full_file_or_folder_name + "/" + index_file_data[index_file_item]).replace("\\n", "").split(",")[0]
+            file_to_write = open(path_inner_file_to_write, "rb")
+            # Write the contents of opened file to destination BIN and close opened file
+            output_bin.write(file_to_write.read())
+            file_to_write.close()
+            # Add the written file size to a variable for use with padding calculations
+            end_file_size = end_file_size + os.path.getsize(path_inner_file_to_write)
+            index_file_item += 1
+
+        # Add Archive termination before padding
+        output_bin.write(b'\xFF\xFF\xFF\xFF')
+
+        # Calculate end padding and write it
+        end_padding = (((end_file_size // padding) + 1) * padding) - end_file_size - 4
+        output_bin.write(end_padding * b'\x00')
+
+        output_bin.close()
+    return
+
+
 help_msg = ("DashEditor v0.2 - Mega Man Legends Hacking Suite\n"
             "Created by _Ombra_ of SadNES cITy Translations\n"
             "Website: http://www.sadnescity.it\n\n"
@@ -216,58 +203,56 @@ elif sys.argv[1] != "-e" and sys.argv[1] != "-c":
 # If the command argument is valid, check if file exists and open it
 elif not os.path.exists(sys.argv[2]):
     print("File or folder not found")
+
 # If second argument is -e and third argument is file
 elif sys.argv[1] == "-e" and not os.path.isfile(sys.argv[2]):
     print("Expected file. Provided folder")
-elif sys.argv[1] == "-e" and os.path.isfile(sys.argv[2]):
-    file_name: str = sys.argv[2]
-    file = open(file_name, "rb")
-    file_data: bytes = file.read()
-
-    # Check if the file is a valid MML BIN file
-    try:
-        assert file_data[64:67].decode("ascii") == "..\\"
-    except AssertionError:
-        print("Not a valid MML PSX BIN file\n")
-
-    # Normalize slashes if file is in sub-folders in Windows
-    # Ex: TEST\\TEST2\\FILE.BIN == TEST/TEST2/FILE.BIN
-    file_name = file_name.replace('\\', '/')
+elif sys.argv[1] == "-c" and not os.path.isdir(sys.argv[2]):
+    print("Expected folder. Provided file")
+else:
+    # Ex: TEST/TEST2/FILE.BIN or TEST/TEST2
+    full_file_or_folder_name: str = sys.argv[2].replace("\\", "/")
+    # Get only the paths from the normalized path
+    # Ex: TEST/TEST2/FILE.BIN == TEST/TEST2/FILE
+    full_path_and_file_no_ext = os.path.splitext(full_file_or_folder_name)[0]
     # Get only the file name from the normalized path
     # Ex: TEST/TEST2/FILE.BIN == FILE.BIN
-    file_name_only = os.path.split(file_name)[-1]
-    # Get only the paths
-    # Ex: TEST/TEST2/FILE.BIN == TEST/TEST2/FILE
-    file_path = file_name.replace(".BIN", "")
+    file_name_only = os.path.basename(full_file_or_folder_name)
 
-    # Create index file
-    if not os.path.exists(file_name.replace(".BIN", "")):
-        os.mkdir(file_name.replace(".BIN", ""))
-
-    index_file = open(file_path + "/" + file_name_only.replace(".BIN", ".txt"), "w+")
-
-    # Proceed with extraction
-    do_unpack_bin()
-    file.close()
-
-# If argument is -c, pack BIN files
-elif sys.argv[1] == "-c" and os.path.isdir(sys.argv[2]):
-
-    # Normalize slashes if file is in sub-folders
-    # Ex: TEST\\TEST2 == TEST/TEST2
-    folder_name: str = sys.argv[2].replace('\\', '/')
-    # Get only the index file name from the normalized path
-    # Ex: TEST/TEST2 == TEST.txt
-    index_file_name = sys.argv[2].split("/")[-1] + ".txt"
     # Full path to the index file
-    index_full_path = folder_name + "/" + index_file_name
+    # Ex: TEST/TEST2 == TEST/TEST2/TEST2.txt
+    index_file_path = full_path_and_file_no_ext + "/" + file_name_only.replace(".BIN", "") + ".txt"
 
-    if not os.path.exists(index_full_path):
-        print("Index file missing")
-    elif os.path.exists(folder_name + ".BIN"):
-        print("BIN file already exists")
-    else:
-        index_file = open(index_full_path, "r")
-        index_file_data: list = index_file.readlines()
-        index_file.close()
-        do_pack_bin()
+    if sys.argv[1] == "-e" and os.path.isfile(sys.argv[2]):
+        file = open(full_file_or_folder_name, "rb")
+        file_data: bytes = file.read()
+
+        # Check if the file is a valid MML BIN file
+        try:
+            assert file_data[64:67].decode("ascii") == "..\\"
+        except AssertionError:
+            print("Not a valid MML PSX BIN file\n")
+
+        # Create index file
+        if not os.path.exists(full_path_and_file_no_ext):
+            os.mkdir(full_path_and_file_no_ext)
+
+        index_file = open(index_file_path, "w+")
+
+        # Proceed with extraction
+        do_unpack_bin()
+        file.close()
+
+    # If argument is -c, pack BIN files
+
+    elif sys.argv[1] == "-c" and os.path.isdir(sys.argv[2]):
+
+        if not os.path.exists(index_file_path):
+            print("Index file missing")
+        elif os.path.exists(full_path_and_file_no_ext + "/" + file_name_only + ".BIN"):
+            print("BIN file already exists")
+        else:
+            index_file = open(index_file_path, "r")
+            index_file_data: list = index_file.readlines()
+            index_file.close()
+            do_pack_bin()
